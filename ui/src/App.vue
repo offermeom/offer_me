2229e021430d27957a72accb367cf242cd0566d6
Lebin Jose <offermeom@gmail.com>
2024-01-22 23:26:14 +0530

    initial commit

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <!-- Your main content goes here -->
+      <router-view></router-view>
     </v-main>
+
+    <!-- Bottom navigation menu -->
+    <v-bottom-navigation v-model="value" active color="primary">
+      <v-btn>
+        <v-icon>mdi-history</v-icon>
+
+        Recents
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-heart</v-icon>
+
+        Favorites
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>Nearby</span>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
-
-  data: () => ({
-    //
-  }),
+  data: () => ({ value: 0 }),
 };
 </script>
+
+<style>
+/* Add your styles as needed */
+</style>
---
+++ ui/src/views/ChoiceScreen.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container class="ma-auto">
+    <div class="c-screen d-flex justify-center">
+      <div>
+        <v-btn
+          depressed
+          color="primary"
+          class="d-block mx-auto my-auto white--text"
+          width="150px"
+          @click="goToVendorLogin()"
+        >
+          Vendor
+        </v-btn>
+        <div class="d-flex justify-center">
+          <div class="my-4 d-inline-flex align-center">
+            <v-divider width="55px"></v-divider>
+            <h4 class="text-center grey--text mx-2">Or</h4>
+            <v-divider width="55px"></v-divider>
+          </div>
+        </div>
+        <v-btn
+          depressed
+          color="primary"
+          outlined
+          class="d-block mx-auto my-auto"
+          width="150px"
+          @click="goToCustomerLogin()"
+        >
+          Customer
+        </v-btn>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToVendorLogin() {
+      this.$router.push("/loginvendor");
+    },
+    goToCustomerLogin() {
+      this.$router.push("/logincustomer");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  padding: 0px !important;
+}
+</style>
